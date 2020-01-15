1ac51e744c6f4a5694652d0b7645caa88d275b70
Talitha <talkruger9@gmail.com>
2020-01-15 21:19:48 +0200

    -Talitha added a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return ( 
-    <div className = "App" >
-    <p > I am making a titivate change </p>
-    </div >
+  return (
+    <div className="App">
+      <p> I am making a titivate change </p>
+      <h1>THis is a Heading</h1>
+      <p>WOrld HEllO</p>
+      <button type="button">Press me</button>
+    </div>
   );
 }
 
