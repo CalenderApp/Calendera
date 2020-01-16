4fc438099ddf5194441bbce9bc28d43178cab79e
Talitha <talkruger9@gmail.com>
2020-01-16 17:18:05 +0200

    -updated button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p> I am making a titivate change </p>
-      <h1>THis is a Heading</h1>
-      <p>WOrld HEllO</p>
-      <button type="button">Does this work</button>
+      <button type="button">Does this work?</button>
     </div>
   );
 }
